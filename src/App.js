2b8825f0409cd5b4acd29d92ce98b8602e2c0663
Sebas <sebas.warsaw@gmail.com>
2020-06-20 19:28:39 -0300

    ctx with provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Loadable from 'react-loadable';
 
+import { ProductProvider } from './state/context';
 import './styles/index.scss';
 
 const LoadableHome = Loadable({
@@ -14,24 +16,48 @@ const LoadableAbout = Loadable({
   loading: () => <div>Loading...</div>
 });
 
-const App = () => (
-  <div className="app">
-    <nav aria-label="main navigation">
-      <NavLink exact to="/" activeClassName="active">
-        Home
-      </NavLink>{' '}
-      <NavLink exact to="/about" activeClassName="active">
-        About
-      </NavLink>
-    </nav>
-    <main className="main">
-      <Switch>
-        <Route exact path="/" component={LoadableHome} />
-        <Route path="/about" component={LoadableAbout} />
-      </Switch>
-    </main>
-    <footer />
-  </div>
-);
+const LoadableProducts = Loadable({
+  loader: () =>
+    import(/* webpackChunkName: 'product' */ './domain/product/Product'),
+  loading: () => <div>Loading...</div>
+});
+
+const App = props => {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.ssrData) {
+      delete window.ssrData;
+    }
+  });
+  const { data } = props;
+  return (
+    <div className="app">
+      <nav aria-label="main navigation">
+        <NavLink exact to="/" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink exact to="/about" activeClassName="active">
+          About
+        </NavLink>
+        <NavLink exact to="/product" activeClassName="active">
+          Product
+        </NavLink>
+      </nav>
+      <main className="main">
+        <Switch>
+          <Route exact path="/" component={LoadableHome} />
+          <Route path="/about" component={LoadableAbout} />
+          <ProductProvider ssrData={data}>
+            <Route path="/product" component={LoadableProducts} />
+          </ProductProvider>
+        </Switch>
+      </main>
+      <footer />
+    </div>
+  );
+};
+
+App.contextTypes = {
+  data: PropTypes.array
+};
 
 export default App;
